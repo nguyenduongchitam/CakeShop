62cc0098201cd3dfce928a1bea33230c4e81f649
DinhZang <nguyentruongdinhgiang@gmail.com>
2023-11-08 23:21:40 +0700

    gui file

---
+++ frontend/modules/aboutuspage.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>trang khách hàng</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> 
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
+    <link rel="stylesheet" href="../../frontend/css/aboutuspage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+     <head>
+  <body>
+  <div class="wrapper">
+    <div class="infor">
+        <img src="../../frontend/modules/menupage/aboutus.jpg" alt="image" width="100%">
+        <p>About Us</p>
+        <p>IU LÀ ĐÂY</p>
+    </div>
+    <div class="feel">
+        <p>Chúng mình - nhứng thợ làm bánh thủ công luôn cố gắng tôn trọng tính nguyên bản, tự nhiên và chân thật nhất của từng nguyên liệu. Để dù có những bất toàn trong mỗi thứ riêng rẽ nhưng sau cùng vẫn tạo nên một ổ bánh hài hòa và ngon nhất</p>
+        <p>Vậy nên, bất cứ khi nào bạn cần những hương vị mộc mạc, tinh tế nhưng chẳng kém phần hấp dẫn, hãy đến với chúng mình. Luôn có rất nhiều điều đặc biệt đợi bạn khám phá đó nhé!</p>
+    </div>
+    <hr>
+  </div>
+  </body>
+</html>
