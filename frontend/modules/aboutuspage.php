15364020b434b9c70895eff453d6a4208f3a344a
DinhZang <nguyentruongdinhgiang@gmail.com>
2023-11-11 22:30:50 +0700

    gui file

--- frontend/modules/aboutuspage.php
+++ frontend/modules/aboutuspage.php
@@ -8,19 +8,32 @@
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
     <link rel="stylesheet" href="../../frontend/css/aboutuspage.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushina">
      <head>
   <body>
-  <div class="wrapper">
+  
+    <img src="../../frontend/modules/menupage/aboutus.jpg" alt="image" width="100%" height="auto">
+    <hr>
+    <hr>
+    <hr>
+    <div class="wrapper">
     <div class="infor">
-        <img src="../../frontend/modules/menupage/aboutus.jpg" alt="image" width="100%">
-        <p>About Us</p>
-        <p>IU LÀ ĐÂY</p>
+        <p class="one">About Us</p>
+        <p class="two"><i>Iu Là Đây</i></p>
     </div>
     <div class="feel">
-        <p>Chúng mình - nhứng thợ làm bánh thủ công luôn cố gắng tôn trọng tính nguyên bản, tự nhiên và chân thật nhất của từng nguyên liệu. Để dù có những bất toàn trong mỗi thứ riêng rẽ nhưng sau cùng vẫn tạo nên một ổ bánh hài hòa và ngon nhất</p>
-        <p>Vậy nên, bất cứ khi nào bạn cần những hương vị mộc mạc, tinh tế nhưng chẳng kém phần hấp dẫn, hãy đến với chúng mình. Luôn có rất nhiều điều đặc biệt đợi bạn khám phá đó nhé!</p>
+        <p><i>Chúng mình - những thợ làm bánh thủ công luôn cố gắng tôn trọng tính nguyên bản, tự nhiên và chân thật nhất của từng nguyên liệu. Để dù có những bất toàn trong mỗi thứ riêng rẽ nhưng sau cùng vẫn tạo nên một ổ bánh hài hòa và ngon nhất.</i></p>
+        <p><i>Vậy nên, bất cứ khi nào bạn cần những hương vị mộc mạc, tinh tế nhưng chẳng kém phần hấp dẫn, hãy đến với chúng mình. Luôn có rất nhiều điều đặc biệt đợi bạn khám phá đó nhé!</i></p>
+        <p></p>
+        <p></p>
+        <p></p>
     </div>
     <hr>
+    <div class="map">
+        <p class="mp">Bản Đồ</p>
+        <p ><iframe class="maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.231240416692!2d106.80047917465674!3d10.870008889284488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527587e9ad5bf%3A0xafa66f9c8be3c91!2sUniversity%20of%20Information%20Technology%20-%20VNUHCM!5e0!3m2!1sen!2s!4v1699713039627!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></p>
+    </div>
   </div>
   </body>
 </html>
