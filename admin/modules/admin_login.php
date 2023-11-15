1a457b0f1aa9d9eaa3cfbcdc11d27d9b5cad6c56
mtoanng <nguyendungmanhtoan@gmail.com>
2023-11-15 13:51:51 +0700

    admin_login

--- admin/modules/admin_login.php
+++ admin/modules/admin_login.php
@@ -4,7 +4,7 @@
     if(isset($_POST['dangnhap'])){
         $email = $_POST['email'];
         $password = $_POST['password'];
-        $sql = "SELECT * FROM user where email='".$email."'AND password='".$password."' LIMIT 1 ";
+        $sql = "SELECT * FROM user where email='".$email."'AND password='".$password."'";
         $row = mysqli_query($mysqli,$sql);
         $count = mysqli_num_rows($row);
         if($count>0)
@@ -27,9 +27,9 @@
     <h2>Đăng nhập trang quản trị</h2>
     <form action="" method="post">
     <table>
-    <tr><input type="text" placeholder="Nhập email" ></tr>
+    <tr><input type="text" placeholder="Nhập email" name="email"></tr>
     <br>    
-    <tr><input type="password" placeholder="Nhập mật khẩu" ></tr>
+    <tr><input type="password" placeholder="Nhập mật khẩu" name="password" ></tr>
     <br>
     <tr><button name="dangnhap">Đăng nhập</button></tr>
     </table>
