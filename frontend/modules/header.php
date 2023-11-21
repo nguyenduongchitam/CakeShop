71b864ae1a6026cf2694e3356c7320be5fed9c4d
KhanhAn <ambrew2003@gmail.com>
2023-11-21 13:35:27 +0700

    updatelogin

--- frontend/modules/header.php
+++ frontend/modules/header.php
@@ -6,16 +6,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="../css/header.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
 
 <body>
+
   <section class="myheader" style="background-color: #ffbdc4;">
     <div class=" container-fluid fs-4 text-center headerig" style="padding: 0;">
       <div class="row align-items-center">
         <div class="col-md-2"><img src="../modules/homepage/2e.png" class="img-fluid logo" alt="Logo" style="max-width: 140px; height:90px"></div>
-        <div class="col-md-5">
+        <div class="col-md-6">
           <section class="menu">
             <div class="container-fluid pt-3 navbarne">
               <div class="row">
@@ -56,16 +57,42 @@
             </div>
           </section>
         </div>
-        <div class="col-md-2" style="padding-top: 18px;">
+        <div class="col-md-2" style="padding-top: 20px;">
           <div class="input-group mb-3">
             <input type="text" class="form-control" style="color: #4d2600; font-family: Segoe UI;" placeholder="Tìm kiếm..." aria-label="Recipient's username" aria-describedby="button-addon2">
             <button class="btn btn-outline-secondary" style="color: #4d2600;" type="button" id="button-addon2"><i class="bi bi-search-heart"></i></button>
           </div>
         </div>
-        <div class="col-md-3">
-          <a href="#" class="col-md-9 account "><i class="bi bi-person-circle bigger-icon"></i> Đăng Nhập</a>
-          <a href="#" class="account"> Đăng Ký</a>
-          <a href="#" class="col-md-3 account bigger-icon" style="padding-left: 20px;"><i class="bi bi-basket2"></i></a>
+        <div class="col-md-2">
+          <section class="menu">
+            <div class="container-fluid pt-3 navbarne">
+              <div class="row">
+                <nav class="navbar navbar-expand-lg" style="padding-top: 0; padding-bottom: 0">
+                  <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                      <ul class="navbar-nav ps-3 navbarr">
+                        <li class="nav-item dropdown">
+                          <a class="nav-link" href="#" data-bs-toggle="dropdown" aria-expanded="false" style="color: #4d2600; padding-top: 0;">
+                            <i class="bi bi-person-circle bigger-icon"></i>
+                          </a>
+                          <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Đăng Ký</a></li>
+                            <li><a class="dropdown-item" href="#">Đăng Nhập</a></li>
+                          </ul>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="#" style="padding-top: 0; color:#4d2600;"><i class="bi bi-basket2 account bigger-icon"></i></a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </div>
@@ -113,3 +140,9 @@
     </div>
   </div>
 </section> -->
+
+<!-- <div class="col-md-3">
+  <a href="#" class="col-md-9 account "><i class="bi bi-person-circle bigger-icon"></i> Đăng Nhập</a>
+  <a href="#" class="account"> Đăng Ký</a>
+  <a href="#" class="col-md-3 account bigger-icon" style="padding-left: 20px;"><i class="bi bi-basket2"></i></a>
+</div> -->
