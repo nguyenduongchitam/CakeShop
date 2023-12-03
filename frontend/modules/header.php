ac5aec157df332b9ab976d02d2a782a981028560
KhanhAn <ambrew2003@gmail.com>
2023-12-03 16:25:06 +0700

    khieu

--- frontend/modules/header.php
+++ frontend/modules/header.php
@@ -12,7 +12,7 @@
 
 <body>
 
-  <section class="myheader" style="background-color: #C7DCA7;">
+  <section class="myheader sticky-top" style="background-color: #C7DCA7;">
     <div class=" container-fluid fs-4 text-center headerig" style="padding: 0;">
       <div class="row align-items-center">
         <div class="col-md-2"><img src="../modules/images/Header-logo.png" class="img-fluid logo" alt="Logo" style="max-width: 140px; height:90px"></div>
