dcaf34de52baea3fa8ada71fee07820707dc5488
thongUIT-IS207 <149362664+thongUIT-IS207@users.noreply.github.com>
2023-12-06 19:43:35 +0700

    Update user.php

--- user.php
+++ user.php
@@ -4,14 +4,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="user.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <title>Modal popup html,css</title>
+    <title>User page</title>
 </head>
 <?php
     include('config.php');
-    $sql = "SELECT * FROM user WHERE email='thongbmt789@gmail.com' LIMIT 1";
+    $email = "thongdit880@gmail.com";
+    $sql = "SELECT * FROM user WHERE email='$email' LIMIT 1";
     $row = mysqli_query($mysqli,$sql);
     $result = mysqli_fetch_array($row);
-    $email = "thongbmt789@gmail.com";
 ?>
 <?php
     if(isset($_POST['save-profile'])){
@@ -40,9 +40,9 @@
     {
         $error=[];
         $announce=[];
-        $eventPassword1 = $_POST['new-password'];
-        $eventPassword2 = $_POST['old-password'];
-        if(empty(trim($eventPassword1))||empty(trim($eventPassword2)))
+        $eventPasswordNew = $_POST['new-password'];
+        $eventPasswordOld = $_POST['old-password'];
+        if(empty(trim($eventPasswordOld))||empty(trim($eventPasswordNew)))
         {
             $error['password']['required'] = 'mật khẩu không được để trống';
         }
@@ -50,13 +50,13 @@
             if(empty(trim($_POST['repeat-password'])))
             {
                 $error['repeat_password']['required'] = 'vui lòng nhâp lại mật khẩu';
-                $error['password']['save'] = $eventPassword1;
+                $error['password']['save'] = $eventPasswordNew;
             }
             else
             {
-                if(($_POST['repeat-password'])!= $eventPassword1){
+                if(($_POST['repeat-password'])!= $eventPasswordNew){
                 $error['repeat_password']['compare'] = 'mật khẩu nhập lại không đúng';
-                $error['password']['save'] = $eventPassword1;
+                $error['password']['save'] = $eventPasswordNew;
                 }
             }
         }
@@ -65,15 +65,13 @@
         include('config.php');
         if(isset($_POST['save-account']))
         {
-            $matkhaucu = $_POST['old-password'];
-            $matkhaumoi = $_POST['new-password'];
             $nhaplaimatkhau = $_POST['repeat-password'];
             $sqlpassword = "SELECT * FROM user WHERE email='$email' LIMIT 1";
             $row2 = mysqli_query($mysqli,$sqlpassword);
             $old = mysqli_fetch_array($row2);
             $passwordHashData = $old['password'];
-            if(password_verify($matkhaucu,$passwordHashData)){
-                $passwordHash = password_hash($matkhaumoi,PASSWORD_DEFAULT);
+            if(password_verify($eventPasswordNew,$passwordHashData)){
+                $passwordHash = password_hash($eventPasswordNew,PASSWORD_DEFAULT);
                 $sqlupdate = "UPDATE user SET password = '$passwordHash' WHERE email = '$email'";
                 mysqli_query($mysqli,$sqlupdate);
                 header('location:user.php');
@@ -81,7 +79,7 @@
             else{
                 $error['old-password']['verify']= 'mật khẩu cũ không đúng';
                 $announce['repeat_password']['save'] = $nhaplaimatkhau;
-                $announce['password']['save'] = $eventPassword1;
+                $announce['password']['save'] = $eventPasswordNew;
             }
         }
     }
@@ -89,14 +87,12 @@
 ?>
 <body>
     <div id="wrapper">
-        <div >
+        <div>
             <div>
-                <div><h3> User profile</h3></div>
                 <div class="main">
                     <div class="taskbar">
                         <div id="account-general">
                             <div>
-                            <hr>
                             <div>
                                 <div class="form-group">
                                     <h3>Thông tin tài khoản</h3>
@@ -116,7 +112,7 @@
                                     </tr>
                                 </table>
                                 <div class="form-group">
-                                    <button type="button" class="btn btn-primary" id="update-profile" name="save">CẬP NHẬT</button>
+                                    <button type="button" class="btn btn-primary" id="update-profile" name="save"><b>CẬP NHẬT</b></button>
                                 </div>
                             </div>
                         </div>
@@ -138,7 +134,7 @@
                                     </tr>
                             </table>
                             <div class="text-right mt-3">
-                                <button type="button" class="btn btn-primary" id="update-account" name="save">CẬP NHẬT</button>
+                                <button type="button" class="btn btn-primary" id="update-account" name="save"><b>CẬP NHẬT</b></button>
                             </div>
                         </div>
                     </div>
@@ -150,20 +146,23 @@
                 <div class="modal" id="modal-demo-profile">
                     <div class="modal-header-profile">
                         <h3>Cập nhật thông tin tài khoản</h3>
-                        <button id="btn-close-profile"><i class="fa-solid fa-xmark"></i></button>
+                        <button id="btn-close-profile"><i class="fa-sharp fa-solid fa-circle-xmark fa-2xl"></i></button>
                     </div>
                     <form class="modal-body-profile" method="POST" action="">
-                        <div class="form-group">
-                            <input type="text" placeholder="Họ và tên" name="fullname">
+                        <div class="form-group-modal">
+                            <i class="fa-solid fa-user"></i>
+                            <input type="text" placeholder="Họ và tên" name="fullname" class="form-input">
                         </div>
-                        <div class="form-group">
-                            <input type="text" placeholder="Số điện thoại" name="phone-number">
+                        <div class="form-group-modal">
+                            <i class="fa-solid fa-phone"></i>
+                            <input type="text" placeholder="Số điện thoại" name="phone-number" class="form-input">
                         </div>
-                        <div class="form-group">
-                            <input type="text" placeholder="Địa chỉ" name="address">
+                        <div class="form-group-modal">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <input type="text" placeholder="Địa chỉ" name="address" class="form-input">
                         </div>
                         <div>
-                            <button type="submit" class="btn btn-primary"  name="save-profile">CẬP NHẬT TÀI KHOẢN</button>
+                            <button type="submit" class="btn btn-primary"  name="save-profile"><b>CẬP NHẬT TÀI KHOẢN</b></button>
                         </div>
                     </form>
                 </div>
@@ -172,26 +171,29 @@
                 <div class="modal" id="modal-demo-account">
                     <div class="modal-header-account">
                         <h3>Cập nhật thông tin đăng nhập</h3>
-                        <button id="btn-close-account"><i class="fa-solid fa-xmark"></i></button>
+                        <button id="btn-close-account"><i class="fa-sharp fa-solid fa-circle-xmark fa-2xl"></i></button>
                     </div>
                     <form class="modal-body-account" method="POST" action="">
-                        <div class="form-group">
-                            <input type="text" placeholder="Nhập mật khẩu cũ" name="old-password">   
+                        <div class="form-group-modal">
+                            <i class="fa-solid fa-lock"></i>
+                            <input type="text" placeholder="Nhập mật khẩu cũ" name="old-password" class="form-input">   
                         </div>
                         <?php
                             echo (!empty($error['password']['required']))?'<span class="error" style="color: red">'.$error['password']['required'].'</span>':false;
 
                             echo (!empty($error['old-password']['verify']))?'<span class="error" style="color: red">'.$error['old-password']['verify'].'</span>':false;
                         ?>
-                        <div class="form-group">
-                            <input type="text" placeholder="Nhập mật khẩu mới" name="new-password" value="<?php echo (!empty($error['password']['save']))?'<span>'.$error['password']['save'].'</span>':false;
-                            echo (!empty($announce['password']['save']))?'<span>'.$announce['password']['save'].'</span>':false;?>"> 
+                        <div class="form-group-modal">
+                            <i class="fa-solid fa-lock"></i>
+                            <input type="text" class="form-input" placeholder="Nhập mật khẩu mới" name="new-password" value="<?php echo (!empty($error['password']['save']))? $error['password']['save'] :false;
+                            echo (!empty($announce['password']['save']))? $announce['password']['save'] :false; ?>"> 
                         </div>
                             <?php
                                 echo (!empty($error['password']['required']))?'<span class="error" style="color: red">'.$error['password']['required'].'</span>':false;
                             ?>
-                        <div class="form-group">
-                            <input type="password" placeholder="Nhập lại mật khẩu" name="repeat-password" value="<?php echo (!empty($announce['repeat_password']['save']))?'<span>'.$announce['repeat_password']['save'].'</span>':false;?>">  
+                        <div class="form-group-modal">
+                            <i class="fa-solid fa-lock"></i>
+                            <input type="password" class="form-input" placeholder="Nhập lại mật khẩu" name="repeat-password" value="<?php echo (!empty($announce['repeat_password']['save']))? $announce['repeat_password']['save']:false;?>">  
                         </div>
                         <?php
                             echo (!empty($error['repeat_password']['required']))?'<span class="error" style="color: red">'.$error['repeat_password']['required'].'</span>':false;
@@ -199,7 +201,7 @@
                             echo (!empty($error['repeat_password']['compare']))?'<span class="error" style="color: red">'.$error['repeat_password']['compare'].'</span>':false;
                         ?>
                         <div>
-                            <button type="submit" class="btn btn-primary" name="save-account">CẬP NHẬT MẬT KHẨU</button>
+                            <button type="submit" class="btn btn-primary" name="save-account"><b>CẬP NHẬT MẬT KHẨU</b></button>
                         </div>
                     </form>
                 </div>
