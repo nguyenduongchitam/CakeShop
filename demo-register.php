cdf8e044d390ec9c2c0eed17bfb4cd79a569e9c8
thongUIT-IS207 <149362664+thongUIT-IS207@users.noreply.github.com>
2023-12-10 17:15:20 +0700

    Update demo-register.php

--- demo-register.php
+++ demo-register.php
@@ -5,37 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="app.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <title>sign in</title>
+    <title>register</title>
 </head>
 <?php
     if($_SERVER['REQUEST_METHOD']=='POST')
     {
         $error=[];
-        if(empty(trim($_POST['password'])))
+        $announce=[];
+        $eventPassword = $_POST['password'];
+        if(empty(trim($eventPassword)))
         {
             $error['password']['required'] = 'mật khẩu không được để trống';
         }
-
-        if(empty(trim($_POST['email'])))
+        else{
+            if(empty(trim($_POST['repeat_password'])))
+            {
+                $error['repeat_password']['required'] = 'vui lòng nhâp lại mật khẩu';
+                $error['password']['save'] = $eventPassword;
+            }
+            else
+            {
+                $matkhau = $_POST['password'];
+                if(($_POST['repeat_password'])!= $matkhau){
+                $error['repeat_password']['compare'] = 'mật khẩu nhập lại không đúng';
+                $error['password']['save'] = $eventPassword;
+                }
+                else{
+                    $announce['repeat_password']['save'] = $matkhau;
+                }
+            }
+        }
+        $eventEmail = $_POST['email'];
+        if(empty(trim($eventEmail)))
         {
             $error['email']['required'] = 'email không được để trống';
         }
-        else
-        {
+        else{
             if(!filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL)){
                 $error['email']['invalid'] = 'email không hợp lệ';
             }
-        }
-
-        if(empty(trim($_POST['repeat_password'])))
-        {
-            $error['repeat_passwor']['required'] = 'vui lòng nhâp lại mật khẩu';
-        }
-        else
-        {
-            $matkhau = $_POST['password'];
-            if(($_POST['repeat_password'])!= $matkhau){
-                $error['repeat_password']['compare'] = 'mật khẩu không đúng';
+            else{
+                $announce['email']['save'] = $eventEmail;
             }
         }
 
@@ -49,13 +59,19 @@
 					$kt=mysqli_query($mysqli, $sql);
 
 					if(mysqli_num_rows($kt)  > 0){
-						echo "Tài khoản đã tồn tại";
+						$error['email']['exist'] = 'email đã tồn tại';
 					}else{  $sql = "INSERT INTO user(
 	    					email,
-                            password
+                            password,
+                            role_id,
+                            create_at,
+                            update_at
 	    					) VALUES (
 	    					'$email',
-	    					'$hashPassword'
+	    					'$hashPassword',
+                            1,
+                            now(),
+                            now()
 	    					)";
                         mysqli_query($mysqli,$sql);
 				   		header('location:sign-in.php');
@@ -71,37 +87,39 @@
     <div id="wrapper">
         <form action="" id="form-login" method="POST">
             <h1 class="form-heading">Đăng ký</h1>
-            <div class="form-group">
-                <i class="fa-regular fa-user"></i>
-                <input type="text" class="form-input" placeholder="Email" name="email" id="email">
-            </div>
-            <?php
+                <div class="form-group">
+                    <i class="fa-regular fa-user"></i>
+                    <input type="text" class="form-input" placeholder="Email" name="email" id="email" value="<?php echo (!empty($announce['email']['save']))?$announce['email']['save']:false; ?>">
+                </div>
+                <?php
                     echo (!empty($error['email']['required']))?'<span class="error" style="color: red">'.$error['email']['required'].'</span>':false;
 
                     echo (!empty($error['email']['invalid']))?'<span class="error" style="color: red">'.$error['email']['invalid'].'</span>':false;
-            ?>
+
+                    echo (!empty($error['email']['exist']))?'<span class="error" style="color: red">'.$error['email']['exist'].'</span>':false;
+                ?>
             <div class="form-group">
                 <i class="fa-solid fa-key"></i>
-                <input type="text" class="form-input" placeholder="Mật khẩu" name="password" id="password">
+                <input type="text" class="form-input" placeholder="Mật khẩu" name="password" id="password" value="<?php echo (!empty($error['password']['save']))?$error['password']['save']:false; echo (!empty($announce['repeat_password']['save']))?$announce['repeat_password']['save']:false;?>">
             </div>
             <?php
                     echo (!empty($error['password']['required']))?'<span class="error" style="color: red">'.$error['password']['required'].'</span>':false;
             ?>
             <div class="form-group">
                 <i class="fa-solid fa-repeat"></i>
-                <input type="password" class="form-input" placeholder="Nhập lại mật khẩu" name="repeat_password" id="repeat_password">
+                <input type="password" class="form-input" placeholder="Nhập lại mật khẩu" name="repeat_password" id="repeat_password" value="<?php echo (!empty($announce['repeat_password']['save']))?$announce['repeat_password']['save']:false;?>">
                 <div id="eye">
                     <i class="fa-solid fa-eye"></i>
                 </div>
             </div>
             <?php
-                    echo (!empty($error['repeat_password']['required']))?'<span class="error" style="color: red">'.$error['repeat_password']['required'].'</div>':false;
+                    echo (!empty($error['repeat_password']['required']))?'<span class="error" style="color: red">'.$error['repeat_password']['required'].'</span>':false;
 
-                    echo (!empty($error['repeat_password']['compare']))?'<span class="error" style="color: red">'.$error['repeat_password']['compare'].'</div>':false;
+                    echo (!empty($error['repeat_password']['compare']))?'<span class="error" style="color: red">'.$error['repeat_password']['compare'].'</span>':false;
             ?>
             <input type="submit" class="form-submit" value="Đăng ký" name="dangky">
-            <input type="submit" class="form-submit" value="Đăng nhập" name="dangnhap">
-            <div class="support">Trở về</div>
+            <input type="submit" class="form-submit" value="Đăng nhập" name="dangnhap" onclick="Sign()">
+            <div class="support" onclick="Redirect()">Trở về</div>
             <?php
                 echo (!empty($error['dangky']['invalid']))?'<span style="color: red;margin-left:120px">'.$error['dangky']['invalid'].'</span>':false;
             ?>
@@ -123,5 +141,15 @@
         }
     });
 });
+</script>
+<script type="text/javascript">
+            function Redirect() {
+               window.location="homepage.php";
+            }
+</script>
+<script>
+    function Sign(){
+                window.location = "front-end/sign-in.php";
+            }
 </script>
 </html>
