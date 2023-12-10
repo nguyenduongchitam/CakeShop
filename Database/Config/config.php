f0f12f3cd315ae4d3a81c7d9191d6cf4ca573bc8
mtoanng <nguyendungmanhtoan@gmail.com>
2023-12-10 20:00:26 +0700

    abc

--- Database/Config/config.php
+++ Database/Config/config.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost","root","","cakeshop");
+$mysqli = new mysqli("localhost","root","","CakeShop");
 // Check connection
 if ($mysqli -> connect_errno) {
   die("Connection failed: " . $conn->connect_error);
--- frontend/modules/set_coupon.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("config.php");
-require('functions.inc.php');
-
-?>
